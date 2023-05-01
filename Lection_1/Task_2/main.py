b5c0b78499b0eee569f64f64c9861b343cee1208
Dmitry Andriushkov <115940434+DmitryAndriushkov@users.noreply.github.com>
2023-05-01 21:29:02 +0300

    Added completed Task_2 for Lection_1

---
+++ Lection_1/Task_2/main.py
@@ -0,0 +1,4 @@
+userName = input("Greetings, please enter your name: ")
+userSurname = input("Thanks, also your surname: ")
+
+print("Thank you, have a nice day {} {}, see ya!".format(userName, userSurname))
